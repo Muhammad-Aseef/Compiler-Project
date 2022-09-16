a614337fa7fd40815b3f0951d9580dbab59dcd48
Muhammad-Aseef <aseefjawaid123@gmail.com>
2022-09-17 01:30:36 +0500

    opr function updated if temp is empty

--- test.py
+++ test.py
@@ -1,25 +1,23 @@
 
 opr = ['=','<','>']
-oprCheck = 0
+oprCheck = True
 
 def isOpr(current, next):
     if current in opr and next in opr:
         if next == '=':
             current += next
-            oprCheck = 2
-            return current, oprCheck
+            return current
         else:
             return current
     else:
         if current in opr:
             return current
+        return False
 
-
-txt = "<="
+txt = "=>"
 
 for i in range(len(txt)-1):
     temp = isOpr(txt[i], txt[i+1])
-    print(temp[1])
     if temp:
         print(temp)
         print(type(temp))
--- token.py
+++ token.py
@@ -1,5 +1,5 @@
 
-from tabnanny import check
+from pickle import TRUE
 
 
 punct = ['{','}','[',']','(',')',',',':']
@@ -9,6 +9,11 @@ keywords = ['if', 'else', 'while', 'for']
 
 def isPunct(ch):
     if ch in punct:
+        return True
+    return False
+
+def nextOpr(next):
+    if next in opr:
         return True
     return False
     
@@ -22,6 +27,7 @@ def isOpr(current, next):
     else:
         if current in opr:
             return current
+        return False
 
 def isDatatype(ch):
     if ch in datatype:
@@ -43,81 +49,91 @@ for f in file:
     lineCount +=  1
     f = f.strip()
     temp = "" 
+    oprCheck = False
     for i in range(len(f)):
-        # if coming is space that means it's breakpoint
-        # it can be multiple spaces 
-        if not f[i] == " ":
-            if i == len(f)-1:
+        # means double operator occurs
+        if oprCheck:
+            oprCheck = False
+            continue
+        else:
+            # if coming is space that means it's breakpoint
+            # it can be multiple spaces 
+            if not f[i] == " ":
+                if i == len(f)-1:
+                    if not temp:
+                        temp += f[i]
+                        if isPunct(temp):
+                            print("last punctuator", temp)
+                            continue
+                        if isOpr(temp):
+                            print("last operator", temp)
+                            continue
+                        print("empty temp (last): does not match to any function which means it is a identifier", temp)
+                        continue
+                    else:
+                        temp += f[i]
+                        if isKeyword(temp):
+                            print("last keyword", temp)
+                            temp = ""
+                            continue
+                        if isDatatype(temp):
+                            print("last datatype", temp)
+                            temp = ""
+                            continue
+                        print("(last) does not match to any function which means it is a identifier", temp)
+                        continue
+                    
+                # if temp is empty
                 if not temp:
                     temp += f[i]
+                    # for coming 
                     if isPunct(temp):
-                        print("last punctuator", temp)
+                        print("punctuator", temp)
+                        temp = ""
                         continue
-                    if isOpr(temp):
-                        print("last operator", temp)
+                    isOp = isOpr(temp, f[i+1])
+                    if isOp:
+                        print("operator:", isOp)
+                        if len(isOp) == 2:
+                            oprCheck = True
+                        temp = ""
                         continue
-                    print("empty temp (last): does not match to any function which means it is a identifier", temp)
-                    continue
+
+                    # for next 
+                    if isPunct(f[i+1]):
+                        print("next punctuator", temp)
+                        temp = ""
+                        continue
+                    if nextOpr(f[i+1]):
+                        print("next operator", temp)
+                        temp = ""
+                        continue
+                # if temp is not empty
                 else:
                     temp += f[i]
-                    if isKeyword(temp):
-                        print("last keyword", temp)
+                    if isPunct(f[i+1]):
+                        print("next2 punctuator", temp)
                         temp = ""
                         continue
+                    if isOpr(f[i+1]):
+                        print("next2 operator", temp)
+                        temp = ""
+                        continue
+            else:
+                if temp == "":
+                    continue
+                else:
                     if isDatatype(temp):
-                        print("last datatype", temp)
+                        print("space", temp)
                         temp = ""
                         continue
-                    print("(last) does not match to any function which means it is a identifier", temp)
-                    continue
-                
-            # if temp is empty
-            if not temp:
-                temp += f[i]
-                # for coming 
-                if isPunct(temp):
-                    print("punctuator", temp)
+                    if isKeyword(temp):
+                        print("space keyword", temp)
+                        temp = ""
+                        continue
+                    print("space, does not match to any function which means it is a identifier", temp)
                     temp = ""
                     continue
-                if isOpr(temp, f[i+1]):
-                    print("operator", temp)
-                    temp = ""
-                    continue
-                # for next 
-                if isPunct(f[i+1]):
-                    print("next punctuator", temp)
-                    temp = ""
-                    continue
-                if isOpr(f[i+1], f[i+2]):
-                    print("next operator", temp)
-                    temp = ""
-                    continue
-            # if temp is not empty
-            else:
-                temp += f[i]
-                if isPunct(f[i+1]):
-                    print("next2 punctuator", temp)
-                    temp = ""
-                    continue
-                if isOpr(f[i+1]):
-                    print("next2 operator", temp)
-                    temp = ""
-                    continue
-        else:
-            if temp == "":
-                continue
-            else:
-                if isDatatype(temp):
-                    print("space", temp)
-                    temp = ""
-                    continue
-                if isKeyword(temp):
-                    print("space keyword", temp)
-                    temp = ""
-                    continue
-                print("space, does not match to any function which means it is a identifier", temp)
-                temp = ""
-                continue
 
 print("line count: ", lineCount)
 
