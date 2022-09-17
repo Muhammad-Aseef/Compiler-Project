d79b91ff1631b7fdcb96594df2cc5bc84db5ab6f
Muhammad-Aseef <aseefjawaid123@gmail.com>
2022-09-18 01:06:42 +0500

    all functions updated

--- token.py
+++ token.py
@@ -4,6 +4,18 @@ opr = ['=','<','>']
 datatype = ['int', 'float', 'string', 'arr']
 keywords = ['if', 'else', 'while', 'for', 'print']
 
+def checkAll(temp, p):
+        if isPunct(temp):
+            print(f"{p}: punctuator {temp}")
+        elif isOpr(temp, 0):
+            print(f"{p}: operator {temp}")
+        elif isKeyword(temp):
+            print(f"{p}: keyword {temp}")
+        elif isDatatype(temp):
+            print(f"{p}: datatype {temp}")
+        else:
+            print(f"{p}: does not match to any function which means it is a identifier: {temp}")
+
 def isPunct(ch):
     if ch in punct:
         return True
@@ -67,12 +79,7 @@ for f in file:
                             print("\" last occurs:", temp) # token for " because it is at last position
                             continue
                         temp += f[i]
-                        if isPunct(temp):
-                            print("last punctuator", temp)
-                        elif isOpr(temp, 0):
-                            print("last operator", temp)
-                        else:
-                            print("empty temp (last): does not match to any function which means it is a identifier:", temp)
+                        checkAll(temp, 'e-l')
                         continue
                     else:
                         if f[i] == '"':
@@ -80,22 +87,16 @@ for f in file:
                             print(f[i]) # token for "
                             continue
                         temp += f[i]
-                        if isKeyword(temp):
-                            print("last keyword", temp)
-                        elif isDatatype(temp):
-                            print("last datatype", temp)
-                        else:
-                            print("not empty (last) does not match to any function which means it is a identifier:", temp)
+                        checkAll(temp, 'n-l')
                         continue
                     
                 # if temp is empty
                 if not temp:
+                    temp += f[i]
                     if f[i] == '"':
-                        temp += f[i]
                         print("\" occurs:", temp)
                         quotation = True
                         continue
-                    temp += f[i]
                     # for coming 
                     if isPunct(temp):
                         print("punctuator", temp)
@@ -108,9 +109,9 @@ for f in file:
                             oprCheck = True
                         temp = ""
                         continue
-
                     # for next 
                     if isPunct(f[i+1]):
+                        # token for identifier because coming value is of length 1 and is not punct or opr
                         print("next punctuator", temp)
                         temp = ""
                         continue
@@ -121,53 +122,84 @@ for f in file:
                 # if temp is not empty
                 else:
                     if f[i] == '"':
-                        print("\" not empty occurs:", temp) # token for temp
-                        if isDatatype(temp):
-                            print("not empty Data type", temp) # token
-                        elif isKeyword(temp):
-                            print("not empty keyword", temp) # token
-                        else:
-                            print("not empty does not match to any function which means it is a identifier:", temp)
+                        checkAll(temp, 'n-q') # token for temp
                         temp = f[i] # temp is used for token now " will over write temp
                         quotation = True
                         continue
                     temp += f[i]
+                    # checking next to be punct or opr
                     if isPunct(f[i+1]):
-                        print("not empty next punctuator", temp)
-                        if isDatatype(temp):
-                            print("not empty Data type", temp) # token
-                        elif isKeyword(temp):
-                            print("not empty keyword", temp) # token
-                        else:
-                            print("not empty does not match to any function which means it is a identifier:", temp)
+                        checkAll(temp, 'n-n-p')
                         temp = ""
                         continue
                     if isOpr(f[i+1], 0):
-                        print("not empty next operator", temp)
-                        if isDatatype(temp):
-                            print("not empty Data type", temp) # token
-                        elif isKeyword(temp):
-                            print("not empty keyword", temp) # token
-                        else:
-                            print("not empty does not match to any function which means it is a identifier", temp)
+                        checkAll(temp, 'n-n-o')
                         temp = ""
                         continue
             else:
                 if temp == "":
                     continue
                 else:
-                    if isDatatype(temp):
-                        print("space", temp)
-                        temp = ""
-                        continue
-                    if isKeyword(temp):
-                        print("space keyword", temp)
-                        temp = ""
-                        continue
-                    print("space, does not match to any function which means it is a identifier", temp)
+                    checkAll(temp, 's')
                     temp = ""
                     continue
 
+
 print("line count: ", lineCount)
 
-            
+'''
+# last
+if isKeyword(temp):
+    print("last keyword", temp)
+elif isDatatype(temp):
+    print("last datatype", temp)
+else:
+    print("not empty (last) does not match to any function which means it is a identifier:", temp)
+
+if isPunct(temp):
+    print("last punctuator", temp)
+elif isOpr(temp, 0):
+    print("last operator", temp)
+else:
+    print("empty temp (last): does not match to any function which means it is a identifier:", temp)
+
+
+# not empty
+print("\" not empty occurs:", temp) # token for temp
+if isDatatype(temp):
+    print("not empty Data type", temp) # token
+elif isKeyword(temp):
+    print("not empty keyword", temp) # token
+else:
+    print("not empty does not match to any function which means it is a identifier:", temp)
+
+print("not empty next punctuator", temp)
+if isDatatype(temp):
+    print("not empty Data type", temp) # token
+elif isKeyword(temp):
+    print("not empty keyword", temp) # token
+else:
+    print("not empty does not match to any function which means it is a identifier:", temp)
+
+print("not empty next operator", temp)
+if isDatatype(temp):
+    print("not empty Data type", temp) # token
+elif isKeyword(temp):
+    print("not empty keyword", temp) # token
+else:
+    print("not empty does not match to any function which means it is a identifier", temp)
+
+
+# space
+if isDatatype(temp):
+    print("space", temp)
+    temp = ""
+    continue
+if isKeyword(temp):
+    print("space keyword", temp)
+    temp = ""
+    continue
+print("space, does not match to any function which means it is a identifier", temp)
+temp = ""
+continue
+'''
