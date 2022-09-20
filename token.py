0564f1b9006788170537f824a7ef3059f18410ea
Muhammad-Aseef <aseefjawaid123@gmail.com>
2022-09-20 22:13:18 +0500

    token class added

--- token.py
+++ token.py
@@ -1,5 +1,15 @@
+import pickle
 
-punct = ['{','}','[',']','(',')',',',':',]
+
+class token:
+    def __init__(self, value, type, line):
+        self.value = value
+        self.type = type
+        self.line = line
+
+
+tokens = []
+punct = ['{','}','[',']','(',')',',',':']
 opr = ['=','<','>']
 airth = ['+', '-', '*', '/']
 inc_dec = ['++', '--']
@@ -10,18 +20,25 @@ keywords = ['if', 'else', 'while', 'for', 'print']
 def checkAll(temp, p):
         if isPunct(temp):
             print(f"{p}: punctuator {temp}")
+            tokens.append(token(temp,'punctuator',lineCount))
         elif isOpr(temp, 0):
             print(f"{p}: operator {temp}")
+            tokens.append(token(temp,'operator',lineCount))
         elif isAirth(temp, 0):
             print(f"{p}: airthmatic {temp}")
+            tokens.append(token(temp,'airthmatic operator',lineCount))
         elif isLogOpr(temp, 0):
             print(f"{p}: logical {temp}")
+            tokens.append(token(temp,'logical operator',lineCount))
         elif isKeyword(temp):
             print(f"{p}: keyword {temp}")
+            tokens.append(token(temp,'keyword',lineCount))
         elif isDatatype(temp):
             print(f"{p}: datatype {temp}")
+            tokens.append(token(temp,'data type',lineCount))
         else:
             print(f"{p}: does not match to any function which means it is a identifier: {temp}")
+            tokens.append(token(temp,'regex',lineCount))
 
 def isPunct(ch):
     if ch in punct:
@@ -72,7 +89,12 @@ def isKeyword(ch):
         return True
     return False
 
+# to read code file
 file = open('E:\\6thSem\\compiler\\myproject\\file.txt','r')
+
+# to write token objects
+# t_file = open('E:\\6thSem\\compiler\\myproject\\token.txt','a')
+
 lineCount = 0
 temp = "" 
 oprCheck = False # checking for both airthmatic and relational operators
@@ -95,19 +117,22 @@ for f in file:
             temp += f[i]
             if i == len(f)-1 or f[i] == '"':
                 quotation = False
-                print("in Quotes:",temp)  # token will be generated for str
+                print("in Quotes:",temp)  # token will be generated for str 
+                tokens.append(token(temp,'quotation',lineCount))
                 temp = ""
         elif inline_comment:
             temp += f[i]
             if i == len(f)-1:
                 inline_comment = False
                 print("line ends",temp)  # token will be generated for inline_comment str
+                tokens.append(token(temp,'inline comment',lineCount))
                 temp = ""
         elif comment:
             temp += f[i]
             if f[i] == '~':
                 comment = False
                 print("comment end",temp)  # token will be generated for inline_comment str
+                tokens.append(token(temp,'comment',lineCount))
                 temp = ""
         else:
             # if coming is space that means it's breakpoint
@@ -119,10 +144,12 @@ for f in file:
                         temp += f[i]
                         if temp == '"':
                             print("\" last occurs:", temp) # token for " because it is at last position
+                            tokens.append(token(temp,'quotation',lineCount))
                             temp = ""
                             continue
                         if temp == '#':
                             print("# last occurs:", temp) # token for " because it is at last position
+                            tokens.append(token(temp,'inline comment',lineCount))
                             temp = ""
                             continue
                         if temp == '~':
@@ -134,21 +161,28 @@ for f in file:
                     else:
                         if f[i] == '"':
                             print("\" last not empty occurs:", temp) # token for temp
+                            tokens.append(token(temp,'regex',lineCount))
                             print(f[i]) # token for "
+                            tokens.append(token(f[i],'quotation',lineCount))
                             temp = ""
                             continue
                         if f[i] == '#':
                             print("# last not empty occurs:", temp) # token for temp
+                            tokens.append(token(temp,'regex',lineCount))
                             print(f[i]) # token for #
+                            tokens.append(token(f[i],'inline comment',lineCount))
                             temp = ""
                             continue
                         if f[i] == '~':
                             print("comment starts", temp) # token for temp
+                            tokens.append(token(temp,'regex',lineCount))
                             temp = f[i]
                             comment = True
                         if f[i] == '!':
                             print("not operator", temp) #token for temp
+                            tokens.append(token(temp,'regex',lineCount))
                             print(f[i]) # token for !
+                            tokens.append(token(f[i],'logical operator',lineCount))
                             temp = ""
                             continue
                         temp += f[i]
@@ -174,16 +208,19 @@ for f in file:
                     if temp == '!' and f[i+1] == '=':
                         temp += f[i+1]
                         print("not equal operator: ", temp)
+                        tokens.append(token(temp,'not equal',lineCount))
                         oprCheck = True
                         temp = ""
                         continue
                     if isPunct(temp):
                         print("punctuator", temp)
+                        tokens.append(token(temp,"punctuator",lineCount))
                         temp = ""
                         continue
                     isOp = isOpr(temp, f[i+1])
                     if isOp:
                         print("operator:", isOp)
+                        tokens.append(token(isOp,'operator',lineCount))
                         if len(isOp) == 2:
                             oprCheck = True
                         temp = ""
@@ -191,6 +228,7 @@ for f in file:
                     isLogop = isLogOpr(temp, f[i+1])
                     if isLogop:
                         print("logicl operator:", isLogop)
+                        tokens.append(token(isLogop,'logical operator',lineCount))
                         if len(isLogop) == 2:
                             oprCheck = True
                         temp = ""
@@ -199,31 +237,39 @@ for f in file:
                     if isAir:
                         if len(isAir) == 1:
                             print("airthmatic operator:", isAir)
+                            tokens.append(token(isAir,'airthmatic operator',lineCount))
                         else:
                             print("inc_dec:", isAir)
+                            tokens.append(token(isAir,'inc_dec',lineCount))
                             oprCheck = True   
                         temp = ""
                         continue
                     # for next 
+                    # regex will be applied to temp
                     if f[i+1] == '!' and f[i+2] == '=':
                         print("next not equal operator: ", temp)
+                        tokens.append(token(temp,'regex',lineCount))
                         temp = ""
                         continue
                     if isPunct(f[i+1]):
                         # token for identifier because coming value is of length 1 and is not punct or opr
                         print("next punctuator", temp)
+                        tokens.append(token(temp,'regex',lineCount))
                         temp = ""
                         continue
                     if isOpr(f[i+1], 0):
                         print("next operator", temp)
+                        tokens.append(token(temp,'regex',lineCount))
                         temp = ""
                         continue
                     if isAirth(f[i+1], 0):
                         print("next airthmatic operator", temp)
+                        tokens.append(token(temp,'regex',lineCount))
                         temp = ""
                         continue
                     if isLogOpr(f[i+1], 0):
                         print("next logical operator", temp)
+                        tokens.append(token(temp,'regex',lineCount))
                         temp = ""
                         continue
                 # if temp is not empty
@@ -248,6 +294,7 @@ for f in file:
                     # checking next to be punct or opr
                     if f[i+1] == '!' and i != len(f)-2 and f[i+2] == '=':
                         print("not empty next not equal operator: ", temp)
+                        tokens.append(token(temp,'regex',lineCount))
                         temp = ""
                         continue
                     if isPunct(f[i+1]):
@@ -276,3 +323,14 @@ for f in file:
 
 
 print("line count: ", lineCount)
+for i in tokens:
+    print(i.type , i.value)
+
+# token file
+# t_file = open('test.pkl','wb')
+# for j in tokens:
+#     pickle.dump(j, t_file, pickle.HIGHEST_PROTOCOL)
+
+# read_file = open('test.pkl','rb')
+# c = pickle.load(read_file)
+# print(c.type)
